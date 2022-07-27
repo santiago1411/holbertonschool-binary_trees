f57f5c3e9160bb40a5c3963e39d68ee3b91398af
santiago1411 <4547@holbertonschool.com>
2022-07-27 18:29:34 -0500

    binary.h

--- binary_trees.h
+++ binary_trees.h
@@ -14,13 +14,12 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
