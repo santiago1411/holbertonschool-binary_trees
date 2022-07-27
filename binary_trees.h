2bb409ca83e6793f36d9afa1c48fd2262e0f44d4
santiago1411 <4547@holbertonschool.com>
2022-07-27 18:27:51 -0500

    binary.h

--- binary_trees.h
+++ binary_trees.h
@@ -14,15 +14,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}
+};
 
-void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
